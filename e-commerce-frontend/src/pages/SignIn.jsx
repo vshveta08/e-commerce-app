a59d9f490b5fe16f01fde3bed32c50710290b5dc
vshveta08 <vshveta08@gmail.com>
2024-08-25 00:47:52 +0530

    update

--- e-commerce-frontend/src/pages/SignIn.jsx
+++ e-commerce-frontend/src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { ENDPOINT } from "../config/endpoint";
 import { useNavigate } from "react-router-dom";
